80f68b613bf9d00c18c02e2b3b6f548bed324dde
minhntk <minhntk8x@gmail.com>
2017-04-30 18:14:47 +0700

    - Implement skeleton

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { CommentComponent } from './comment/comment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BlogComponent,
+    CommentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
